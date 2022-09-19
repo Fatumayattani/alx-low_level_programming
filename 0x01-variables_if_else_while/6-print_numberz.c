a77311d09dac9235bbd4faa0c3c9d0e60a6c8083
Fatumayattani <fyattani@gmail.com>
2022-09-19 11:15:47 -0700

    numberz commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - prints the numbers from 0 to 9
+*
+* Destription: using the main function
+* this program prints "Programming is positive, zero, or negative
+*Return: 0
+*/
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}

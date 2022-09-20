c6a6c46f35f78f9192293128c461604f55e56c52
Fatumayattani <fyattani@gmail.com>
2022-09-20 12:09:32 -0700

    rev commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* rev_string - reverse the string.
+* @s: pointer to string
+*
+* return: void.
+*/
+void rev_string(char *s)
+{
+
+int i, j, k, temp;
+i = 0;
+while (s[i] l= '\0')
+{
+i++;
+}
+k = 0;
+j = i - l;
+while k < j)
+{
+temp = s[k];
+s[k] = s[j];
+s[j] = temp;
+k++;
+j--;
+}
+}

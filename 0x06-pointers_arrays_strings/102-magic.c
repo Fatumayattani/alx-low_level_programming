bf4353f072c4803ce6599f530072c32c4059f881
Fatumayattani <fyattani@gmail.com>
2022-09-22 11:16:13 -0700

    magic12 commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,41 +1,45 @@
 #include <stdio.h>
 
+
+
 int main(void)
 
 {
 
-int n;
+  int n;
 
-int a[5];
+  int a[5];
 
-int *p;
+  int *p;
 
-a[2] = 1024;
 
-p = &n;
 
-/*
+  a[2] = 1024;
 
-* write your line of code here...
+  p = &n;
 
-* Remember:
+  /*
 
-* you are not allowed to use a
+   * write your line of code here...
 
-* you are not allowed to modify p
+   * Remember:
 
-* only one statement
+   * - you are not allowed to use a
 
-* you are not allowed to code anything else than this line of code
+   * - you are not allowed to modify p
 
-*/
+   * - only one statement
 
-*(p + 5) = 98;
+   * - you are not allowed to code anything else than this line of code
 
-/* ...so that this prints 98\n */
+   */
 
-printf("a[2] = %d\n", a[2]);
+  *(p + 5) = 98;
 
-return (0);
+  /* ...so that this prints 98\n */
+
+  printf("a[2] = %d\n", a[2]);
+
+  return (0);
 
 }

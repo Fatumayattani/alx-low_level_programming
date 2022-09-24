c913e3932368ab12f6457e185532333ad46fa382
Fatumayattani <fyattani@gmail.com>
2022-09-24 11:20:18 -0700

    824 hour commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,31 +1,25 @@
 #include "main.h"
+
 /**
-* jack_bauer - function that prints every minute of the day of jack bauer,starting
-* n from 00:00 to 23:59, min loop counts mins,while hour loop counts hours
-* and resets mins
-* Return: 0
+* jack_bauer - prints every minute of the day
+*
+* Return: the hour from 00:00 to 23:59
 */
 void jack_bauer(void)
 {
-int hours = 0;
-int minutes = 0;
-int hours_remainder;
-int mins_remainder;
-while (hours <= 23)
+int a;
+int b;
+
+for (a = 0 ; a <= 23 ; a++)
 {
-while (minutes <= 59)
+for (b = 0 ; b <= 59 ; b++)
 {
-av
-hours_remainder = hours % 10;
-_putchar(hours / 10 + '0');
-_putchar(hours_remainder + '0')
-_putchar(':');
-_putchar(minutes / 10 +'0');
-_putchar(mins_remainder + '0');
-minutes++;
+_putchar(a / 10 + '0');
+_putchar(a % 10 + '0');
+_putchar(58);
+_putchar(b / 10 + '0');
+_putchar(b % 10 + '0');
 _putchar('\n');
 }
-hours++;
-minutes = 0;
 }
 }

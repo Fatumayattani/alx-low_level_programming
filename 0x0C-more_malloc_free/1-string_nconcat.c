f42584f423220cc7e8e682c718cf95f84f510262
Fatumayattani <fyattani@gmail.com>
2022-10-06 04:48:58 -0700

    nconcat commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,59 +1,43 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
-* string_nconcat - prints concatenate string;
-* @s1: input string.*
-* @s2: input string.
-* @n: len s2 string for print.
-*
-* Return: Nothing.
+* string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
+* 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
+* @s1: input one
+* @s2: input two
+* @n: s2's number of bytes
+* Return: 0
 */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
-		unsigned int x, y, z;
-
-			char *a;
+char *arr;
+unsigned int i, j, co, co_2;
 if (s1 == NULL)
 s1 = "";
-
 if (s2 == NULL)
 s2 = "";
-l1 = 0;
-
-while (s1[x])
-	x++;
-
-
-
-a = malloc(sizeof(*a) * x + n + 1);
-
-if (a == NULL)										return (NULL);
-
-
-
-										for (y = 0, z = 0; y < (x + n); y++)											{
-
-															if (y < x)
-
-																		{
-
-																						a[y] = s1[y];
-
-																								}
-
-																	else
-
-																				{
-
-																								a[y] = s2[z++];
-
-																										}
-
+for (i = 0; s1[i] != '\0'; i++)
+{
 }
-											a[y] = '\0';
-										return (a);
-
+for (j = 0; s2[j] != '\0'; j++)
+{
+}
+if (n < j)
+j = n;
+j += i;
+arr = malloc(sizeof(char *) * (j + 1));
+if (arr == NULL)
+return (NULL);
+for (co = 0; co < i; co++)
+arr[co] = s1[co];
+for (co_2 = 0; co < j; co_2++)
+{
+arr[co] = s2[co_2];
+co++;
+}
+co++;
+arr[co] = '\0';
+return (arr);
 }

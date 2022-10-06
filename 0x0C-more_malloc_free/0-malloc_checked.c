a6f3674dff23a665f2270c4de476f8d4f06dca32
Fatumayattani <fyattani@gmail.com>
2022-10-06 03:03:17 -0700

    malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,17 +2,25 @@
 #include <stdlib.h>
 
 /**
+*
 * malloc_checked - creates an array of integers.
+*
 * @b: input.
+*
 * Return: Output.
+*
 */
+
 void *malloc_checked(unsigned int b)
+
 {
 void *f = malloc(b);
-
 if (f == NULL)
 
+{
 exit(98);
+}
 
 return (f);
+
 }

f64b9daeef613dd232784b0cb73dc58bde05ae82
Fatumayattani <fyattani@gmail.com>
2022-10-06 04:02:10 -0700

    malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,26 +1,16 @@
-#include "main.h"
 #include <stdlib.h>
-
 /**
 *
 * malloc_checked - creates an array of integers.
-*
 * @b: input.
 *
 * Return: Output.
 *
 */
-
 void *malloc_checked(unsigned int b)
-
 {
 void *f = malloc(b);
 if (f == NULL)
-
-{
 exit(98);
-}
-
 return (f);
-
 }
---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+*
+* _putchar - prints the character c to stdout
+*
+* @c: the character to print
+*
+* On error, -1 is returned, and error is set appropriately.
+*
+*/
+
+int _putchar(char c)
+
+{
+
+	return (write(1, &c, 1));
+
+}

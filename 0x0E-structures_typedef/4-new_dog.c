8af3adf46b96daa25ef91bb9756a94dda3746f1c
Fatumayattani <fyattani@gmail.com>
2022-10-11 05:41:29 -0700

    new dog commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,49 +1,37 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
-*new_dog - prints a new dog
-*@name: first member.
-*@age: second member.
-*@owner: third member.
-*
-*Return: NULL if fail.
+* new_dog - entry point
+* @name: string from main, name of pet
+* @age: number from main, age of pet
+* @owner: string from main, owner of pet
+* Return: p
 */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int i, lenN, lenO;
-struct dog *n_dog = NULL;
-1enN = 0;
-while (name[lenN] != '\0')
-lenN++;
-lenO = 0;
-while (owner[lenO] != '\0')
-leno++;
-n_dog == malloc(sizeof(struct dog));
-if (n_dog == NULL)
+dog_t *p;
+/* reserving memory to struct*/
+p = malloc(sizeof(dog_t));
+if (p == NULL)
+return (NULL);
+/* Cpunting name pointer*/
+if (name == NULL)
 {
-free(n_dog);
+free(p);
+free(owner);
 return (NULL);
 }
-n_dog->name = malloc(lenN + 1);
-if (n_dog->name == NULL)
+if (owner == NULL)
 {
-free(n_dog->name);
-free(n_dog);
+free(p);
+free(name);
 return (NULL);
 }
-n_dog->owner = malloc(lenO + 1);
-if (n_dog->owner == NULL)
-{
-free(n_dog->name);
-free(n-dog->owner);
-free(n_dog);
-return (NULL);
-}
-for (i = 0; i <= lenN; i++)
-n_dog->name[i] = name[i];
-for (i = 0; i <= lenO; i++)
-n_dog->owner[i] = owner[i];
-n_dog->age = age;
-return (n_dog);
+p->name = name;
+p->age = age;
+p->owner = owner;
+return (p);
+
 }

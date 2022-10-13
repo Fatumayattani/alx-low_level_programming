9ae7a9848d93d59f9ffae2b3dc8bc1c1305e0265
Fatumayattani <fyattani@gmail.com>
2022-10-13 11:02:50 -0700

    Print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
 *@name: char string
 *@f: function pointer that takes a string argument
 */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *));
 {
 f(name);
 }

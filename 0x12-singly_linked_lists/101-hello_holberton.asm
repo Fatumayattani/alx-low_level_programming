69d9e0824dc8e3f7c9ebef46d9029fbc8f87748d
Fatumayattani <fyattani@gmail.com>
2022-10-24 03:54:25 -0700

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,14 +2,14 @@ SECTION .data
 msg:  db "Hello, Holberton", 0
 fmt:  db "%s", 10, 0
 
-      SECTION .tect
-      extern printf
-      global main
+SECTION .tect
+extern printf
+global main
 main:
-      move esi, msg
-      mov edi, fmt
-      mov eax, 0
-      call printf
+move esi, msg
+mov edi, fmt
+mov eax, 0
+call printf
 
-     mov eax, 0
-     ret
+mov eax, 0
+ret

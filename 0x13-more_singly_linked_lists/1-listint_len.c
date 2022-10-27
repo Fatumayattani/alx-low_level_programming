8abaf08176723e5ebab533a139ed719f8551efdd
Fatumayattani <fyattani@gmail.com>
2022-10-27 02:29:56 -0700

    len list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,8 +6,9 @@
  *
  * Return: number of elements in a linked listint_t list
  */
-size_t listint_len(const listint_t *h)
+size_t listint_len(const listint_t *h);
 {
+
 	size_t count = 0;
 	while (h != NULL)
 	{

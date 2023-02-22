7ef925a56b4fbaf376b337f931c349ecc5b58e74
Fatumayattani <fyattani@gmail.com>
2023-02-22 06:20:15 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
-* main - Entry point
+*main - Print _putchar
 *
-* Return: Always 0 (Success)
+*Return: Always 0(Success)
 */
 int main(void)
 {
-char h[] = "main";
-int m;
-
-m = 0;
-
-while (m <= 8)
-{
-_putchar(h[m]);
-m++;
-}
-_putchar('\n');
-return (0);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
+	return (0);
 }
